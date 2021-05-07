d901c20c522dc07c89bc662cce3b10d9651aea24
Alejandro Schmidt <alemusho@gmail.com>
2021-05-07 00:22:14 -0300

    ok

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,15 +3,18 @@ import { useState } from "react";
 import Header from "./Header";
 import Movies from "./Movies";
 import "./Movies.css";
+import useIsOnline from "../hooks/useIsOnline";
 
 function HomePage() {
   const [inputText, setInputText] = useState("");
   const [rate, setRate] = useState(0);
 
+  const isOnline = useIsOnline();
+
   return (
     <div>
       <Header setInputText={setInputText} setRate={setRate} rate={rate} />
-
+      {isOnline ? "Estás ONline" : "Estás OFFline"}
       <Movies inputText={inputText} rate={rate} />
     </div>
   );
---
+++ src/hooks/useIsOnline.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function useIsOnline() {
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+  function handleConnection() {
+    setIsOnline(window.navigator.onLine);
+  }
+
+  useEffect(() => {
+    window.addEventListener("online", handleConnection);
+    window.addEventListener("offline", handleConnection);
+    return () => {
+      window.removeEventListener("online", handleConnection);
+      window.removeEventListener("offline", handleConnection);
+    };
+  });
+  return isOnline;
+}
+
+export default useIsOnline;
